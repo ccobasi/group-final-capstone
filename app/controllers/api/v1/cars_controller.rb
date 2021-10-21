994e425141210fb2af4b878a0154508c79c5862a
errea <erreakezy@gmail.com>
2021-10-21 23:26:10 +0100

    Fix rubocop errors + modify gem file

--- app/controllers/api/v1/cars_controller.rb
+++ app/controllers/api/v1/cars_controller.rb
@@ -5,38 +5,4 @@ class Api::V1::CarsController < ApplicationController
     @cars = Car.all
     render json: @cars
   end
-  # GET /todos/1
-  # def show
-  #   render json: @todo
-  # end
-  # # POST /todos
-  # def create
-  #   @todo = Todo.new(todo_params)
-  #   if @todo.save
-  #     render json: @todo, status: :created, location: api_v1_todos_path(@todo)
-  #   else
-  #     render json: @todo.errors, status: :unprocessable_entity
-  #   end
-  # end
-  # # PATCH/PUT /todos/1
-  # def update
-  #   if @todo.update(todo_params)
-  #     render json: @todo
-  #   else
-  #     render json: @todo.errors, status: :unprocessable_entity
-  #   end
-  # end
-  # # DELETE /todos/1
-  # def destroy
-  #   @todo.destroy
-  # end
-  # private
-    # Use callbacks to share common setup or constraints between actions.
-    # def set_todo
-    #   @todo = Todo.find(params[:id])
-    # end
-    # # Only allow a list of trusted parameters through.++++++
-    # def todo_params
-    #   params.require(:todo).permit(:task)
-    # end
 end
