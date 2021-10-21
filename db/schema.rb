3fc7c7e645509147e916a4c25771571a2ce1b518
errea <erreakezy@gmail.com>
2021-10-21 23:12:14 +0100

    Modify migrations files + update schema

--- db/schema.rb
+++ db/schema.rb
@@ -41,9 +41,11 @@ ActiveRecord::Schema.define(version: 2021_10_21_154154) do
     t.boolean "reserve"
     t.date "date"
     t.bigint "car_id", null: false
+    t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["car_id"], name: "index_reservations_on_car_id"
+    t.index ["user_id"], name: "index_reservations_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
