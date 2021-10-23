4d632944f776d6975cce5616ff83653fc7f3ba08
Jocyline Magero <jmagero2017@gmail.com>
2021-10-23 17:50:39 -0500

    Add routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,10 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   namespace :api do
     namespace :v1 do
-      resources :cars
-      resources :car_types
-      resources :reservations
+      resources :users, :cars, :car_types, :reservations
+      post :sign_in, to: 'sessions#create'
+      delete :sign_out, to: "sessions#logout"
+      get :signed_in, to: "sessions#logged_in"
     end
   end
 
