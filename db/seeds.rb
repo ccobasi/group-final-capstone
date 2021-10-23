f8a702b1b08d3984d637494244755d9656f3a508
Jocyline Magero <jmagero2017@gmail.com>
2021-10-23 16:11:48 -0500

    Add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,33 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+User.destroy_all
+City.destroy_all
+Car.destroy_all
+Reservation.destroy_all
+
+users = User.create!([
+  {username: 'jocyline', email:'jo@gmail.com', admin:true, password_digest:"jo@8"},
+  {username: 'eri-ngozi', email:'er@gmail.com', admin:false, password_digest:"er@8"},
+  {username: 'chukuma', email:'chuma@gmail.com', admin:false, password_digest:"chuma@8"},
+  {username: 'irenehe', email:'irene@gmail.com', admin:false, password_digest:"iren@8"},
+])
+
+cities = City.create!([
+  {name: 'Kampala'},
+  {name: 'Nairobi'},
+  {name: 'Abuja'},
+  {name: 'Lagos'}
+])
+
+cars = Car.create!([
+  {name: 'corona', image_data: 'helo' },
+  {name: 'gypsum',  image_data: 'hhe'},
+  {name: 'Prado', image_data: 'hhhe'},
+  {name: 'Limozine', image_data: 'hhehe'}
+])
+
+reservations = Reservation.create!([
+  {date: Date.new(), user_id: users[0].id, car_id: cars[0].id, reserve:false},
+  {date: Date.new(), user_id: users[1].id, car_id: cars[1].id, reserve:false},
+  {date: Date.new(), user_id: users[2].id, car_id: cars[2].id, reserve:false},
+  {date: Date.new(), user_id: users[3].id, car_id: cars[3].id, reserve:true}
+])
 
