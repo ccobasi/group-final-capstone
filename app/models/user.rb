7f8ebbb7e6b0c215ae1a7916c6b03e20d7781ec3
Jocyline Magero <jmagero2017@gmail.com>
2021-10-23 16:10:59 -0500

    Add User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+    validates :username, presence: true, length: { minimum: 6, maximum: 12 }, uniqueness: true
+    validates_presence_of :email
+    validates_uniqueness_of :email
+
+    has_many :reservations, dependent: :destroy
+    has_secure_password
 end
