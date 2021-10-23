95a68c474fe58dbd94685c19f46d2e264326d73d
Jocyline Magero <jmagero2017@gmail.com>
2021-10-23 16:11:36 -0500

    Add reservations model

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,7 +1,8 @@
 class Reservation < ApplicationRecord
-  validates :car_id, presence: true
+  validates :date, presence: true
   validates :user_id, presence: true
+  validates :car_id, presence: true
 
-  belongs_to :car
   belongs_to :user
+  belongs_to :car
 end
