918a8a3f3905c93f664a5cda90a3352ee2c5b2ca
Jocyline Magero <jmagero2017@gmail.com>
2021-10-26 12:11:51 -0500

    Add routes for register and sign

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,10 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   namespace :api do
     namespace :v1 do
-      resources :tokens, only: [:create]
-      resources :users, :cars, :car_types, :reservations, :cities
+      post 'register', to: 'users#create'
+      post 'sign_in', to: 'tokens#create'
+      resources :users, only: %[index show update destroy]
+      resources :cars, :car_types, :reservations, :cities
     end
   end
 
